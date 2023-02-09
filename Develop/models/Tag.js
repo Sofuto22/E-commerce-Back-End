0f8ba6c7eb98845e51f6ffe5311497a6691b04ce
Sofuto22 <zenigata19@gmail.com>
2023-02-09 18:13:06 -0500

    did tag.js

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -5,16 +5,25 @@ const sequelize = require('../config/connection.js');
 class Tag extends Model {}
 
 Tag.init(
-  {
-    // define columns
+  {  
+    id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    setPrimayKey: true,
+    autoIncrement: true,
   },
+  tag_Name: {
+    type: DataTypes.STRING
+    // define columns
+    },
+  },  
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'tag',
-  }
+  },
 );
 
 module.exports = Tag;
